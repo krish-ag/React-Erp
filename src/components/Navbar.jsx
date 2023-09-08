f5365dea4157db425f3a2d0434e0f694ccc3b648
Krish Agrawal <printf.krish@gmail.com>
2023-09-08 20:31:05 +0530

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  MdRefresh,
+  MdOutlineHome,
+} from 'react-icons/md';
+
+function Navbar(props) {
+    return (
+      <div className="k-nav">
+          <div className="d-flex justify-content-around align-items-center">
+              <div className="k-nav-page-title">
+                <MdOutlineHome />
+                <span>Home</span>
+                <MdRefresh />
+              </div>
+          </div>
+      </div>
+    );
+}
+
+export default Navbar;
+
+
+/*
+<nav className="navbar navbar-expand-lg navbar-dark k-nav k-blur mb-3">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2 bg-transparent text-light" type="search" placeholder="Search" aria-label="Search" style={{border: "1px solid #fff", borderRadius: "60px"}} />
+        <button className="btn btn-outline-light" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+*/
